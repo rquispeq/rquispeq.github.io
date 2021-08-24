a8cec8e392a547634ba03ea864315fc210a31e64
Rony Quispe <oghmand@gmail.com>
2021-08-24 00:49:53 -0500

    Rediseño de cv.

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+var profile_name = document.getElementById('profile_name')
+profile_name.textContent = 'Rony Martín'
+
+var profile_lastname = document.getElementById('profile_lastname')
+profile_lastname.textContent = 'Quispe Quispe'
